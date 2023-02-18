0c50f935b4dec54bd1b23dfb5453195acda8664a
Ignacio <nachodiaz99@gmail.com>
2023-02-17 22:34:00 -0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    'comma-dangle': ['error', 'only-multiline'],
+    'no-console': 'warn',
+    'no-unused-vars': 'error',
+    'no-extra-semi': 'error',
+    'no-var': 'error',
+    'one-var': ['error', 'never'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always',
+      },
+    ],
+    'one-var-declaration-per-line': ['error', 'always'],
+    'prettier/prettier': 'error',
+  },
+};
---
+++ src/entry-client.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.scss';
+
+ReactDOM.hydrateRoot(
+  document.getElementById('root'),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
